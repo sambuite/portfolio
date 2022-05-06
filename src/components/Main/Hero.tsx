ab2eb839d6c6294467b7f88f326c1a122244bf4b
sambuite <murilosambuite@gmail.com>
2022-05-06 18:03:03 -0300

    update tags

--- src/components/Main/Hero.tsx
+++ src/components/Main/Hero.tsx
@@ -4,13 +4,13 @@ export function Hero() {
   return (
     <section className="flex flex-col default-center mt-56 ">
       <div className="flex justify-between">
-        <span className="text-h-1 whitespace-pre ">
+        <h1 className="text-h-1 whitespace-pre ">
           Olá, sou {'\n'}Murilo Sambuite.
-        </span>
-        <span className="h-fit self-end text-h-2 w-1/2">
+        </h1>
+        <h2 className="h-fit self-end text-h-2 w-1/2">
           Um desenvolvedor frontend que busca criar interfaces precisas e
           performáticas.
-        </span>
+        </h2>
       </div>
       <a href="#" className="h-fit w-fit">
         <ArrowDown className="w-9 h-9 mt-32 " weight="bold" />
