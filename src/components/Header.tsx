112abecea46761d85b07540bb1d50edf34c764ae
sambuite <murilosambuite@gmail.com>
2022-05-06 19:44:05 -0300

    add a function to scroll to a specific section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,9 @@
+export function scrollToProjects() {
+  document
+    .querySelector('#first-project')
+    ?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+}
+
 export function Header() {
   // function toggleTheme() {
   //   const themeMode = localStorage.getItem(THEME_KEY);
@@ -16,7 +22,10 @@ export function Header() {
       <div className="flex default-center mt-14 justify-between">
         <span className="text-4xl font-bold">ms.</span>
         <nav className="self-end ">
-          <a className="text-navigation" href="#">
+          <a
+            className="text-navigation cursor-pointer"
+            onClick={scrollToProjects}
+          >
             Projetos
           </a>
           <a className="text-navigation" href="#">
