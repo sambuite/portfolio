b635add05a23dad7b9c855ce11db0036b4200f16
sambuite <murilosambuite@gmail.com>
2022-05-07 09:48:39 -0300

    add a array to render the projects

--- src/components/Main/Projects/index.tsx
+++ src/components/Main/Projects/index.tsx
@@ -1,39 +1,57 @@
 import homeImg from '../../../assets/home.png';
 import { Project } from './Project';
 
+const projects = [
+  {
+    id: 'first-project',
+    name: '01/BQT - Big Queue Terminator',
+    description:
+      'Essa aplicação visa solucionar o problema das filas em orgãos públicos, possibilitando a marcação de horário antecipado.',
+    imgCover: homeImg,
+    link: {
+      text: 'Acesse o site no ar',
+      url: 'https://github.com/sambuite',
+    },
+    subText:
+      'Criado usando Typescript e muita coisa legal você nem acreditaria se eu te contasse é muito legal mesmo acredite em mim',
+  },
+  {
+    name: '02/Edit anywhere',
+    description: 'SAAS que permite a edição de videos direto do navegador',
+    subText:
+      'Criado usando Javascript e nem tanta coisa legal se eu contasse você acreditaria sim.',
+    imgCover: homeImg,
+    route: {
+      text: 'Acesse já a página',
+      url: 'https://github.com/sambuite',
+    },
+  },
+  {
+    name: '03/Brower',
+    description: 'Apenas um browser escrito em Rust!',
+    imgCover: homeImg,
+    link: {
+      text: 'Baixe aqui',
+      url: 'https://github.com/sambuite',
+    },
+  },
+];
+
 export function Projects() {
   return (
     <section className="default-center flex flex-col mt-36">
-      <Project
-        id="first-project"
-        name="01/BQT - Big Queue Terminator"
-        description="Essa aplicação visa solucionar o problema das filas em orgãos públicos, possibilitando a marcação de horário antecipado."
-        imgCover={homeImg}
-        link={{
-          text: 'Acesse o site no ar',
-          url: 'https://github.com/sambuite',
-        }}
-        subText="Criado usando Typescript e muita coisa legal você nem acreditaria se eu te contasse é muito legal mesmo acredite em mim"
-      />
-      <Project
-        name="02/Edit anywhere"
-        description="SAAS que permite a edição de videos direto do navegador"
-        subText="Criado usando Javascript e nem tanta coisa legal se eu contasse você acreditaria sim."
-        route={{
-          text: 'Acesse já a página',
-          url: 'https://github.com/sambuite',
-        }}
-        imgCover={homeImg}
-      />
-      <Project
-        name="01/Brower"
-        description="Apenas um browser escrito em Rust!"
-        imgCover={homeImg}
-        link={{
-          text: 'Baixe aqui',
-          url: 'https://github.com/sambuite',
-        }}
-      />
+      {projects.map((project, index) => (
+        <Project
+          key={index}
+          id={index === 0 ? 'first-project' : undefined}
+          name={`0${index + 1}/` + project.name}
+          description={project.description}
+          imgCover={project.imgCover}
+          link={project.link}
+          route={project.route}
+          subText={project.subText}
+        />
+      ))}
     </section>
   );
 }
