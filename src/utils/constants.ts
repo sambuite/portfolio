b37a362f115985def4d13d81a9f1e77ad729df07
sambuite <murilosambuite@gmail.com>
2022-05-07 15:54:27 -0300

    fix const value

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,6 @@ const THEME_KEY = '@murilosambuite:theme';
 
 const CONTACTS_ID = 'contacts';
 const PROJECTS_ID = 'projects';
-const HEADER_ID = 'projects';
+const HEADER_ID = 'header';
 
 export { THEME_KEY, CONTACTS_ID, PROJECTS_ID, HEADER_ID };
