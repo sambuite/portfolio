472cafa79e83836c3a7d50ab8a001d81e6a31241
sambuite <murilosambuite@gmail.com>
2022-05-08 08:42:30 -0300

    update button

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -7,10 +7,10 @@ export function BackToTopButton() {
     <button
       type="button"
       title="Voltar ao topo"
-      className="fixed bottom-14 right-14 w-12 h-12"
+      className="fixed flex items-center justify-center rounded-full bottom-14 right-14 w-12 h-12 "
       onClick={() => scrollToSection(HEADER_ID)}
     >
-      <ArrowCircleUp className="w-12 h-12" />
+      <ArrowCircleUp className="w-12 h-12" weight="fill" />
     </button>
   );
 }
